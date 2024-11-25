cc7a9e50a2b0cd5f77baddfa0f9984a97924a069
arturdolzan <arturdolzan@gmail.com>
2024-11-25 18:35:25 -0300

    Override testnet RPC to devnet

--- src/networks/hemiTestnet.ts
+++ src/networks/hemiTestnet.ts
@@ -1,4 +1,7 @@
 import { hemiSepolia } from 'hemi-viem'
 import { overrideRpcUrl } from './utils'
 
-export const hemiTestnet = overrideRpcUrl(hemiSepolia)
+export const hemiTestnet = overrideRpcUrl(
+  hemiSepolia,
+  'https://devnet2.rpc.hemi.network/rpc',
+)
